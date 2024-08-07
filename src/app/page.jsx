10aaf696c9a4bcd17380ed1d9fe4af709b4a321a
KianSayuri <159494737+KianSayuri@users.noreply.github.com>
2024-08-06 22:48:26 -0300

    06/08

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,9 +3,12 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className="containerGlobal">
       <div className={styles.containerTitulo}>
         <h1 className={styles.txtTitulo}>Aula Next</h1>
+        <h2>Aprendendo Next</h2>
+        <h3>Testando</h3>
+        <p>Aprendendo Next</p>
       </div>  
     </div>
   );
--- src/app/usuarios/cadastro/page.jsx
+++ src/app/usuarios/cadastro/page.jsx
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 
 export default function Cadastro() {
   return (
-    <div className="container">
+    <div className="containerGlobal">
       <div className={styles.containerTitulo}>
         <h1 className={styles.txtTitulo}>Cadastro</h1>
       </div>  
