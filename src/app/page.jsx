6662b9f4ad5a8f1a64cfa0d3c5868a85b0df1f8b
KianSayuri <159494737+KianSayuri@users.noreply.github.com>
2024-08-13 21:53:58 -0300

    13/08

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,9 +6,25 @@ export default function Home() {
     <div className="containerGlobal">
       <div className={styles.containerTitulo}>
         <h1 className={styles.txtTitulo}>Aula Next</h1>
-        <h2>Aprendendo Next</h2>
+        <h2 >Aprendendo Next</h2>
         <h3>Testando</h3>
         <p>Aprendendo Next</p>
+        <div className="styles.containerImagem">
+        <Image
+          src="/avre.jpg"
+          width={100}
+          height={100}
+          alt="Imagem de uma Avre"
+          className={styles.imagemHome}
+        />
+        <Image
+          src="/avre.jpg"
+          width={500}
+          height={500}
+          alt="Imagem de uma Avre"
+          className={styles.imagemHome}
+        /> 
+        </div>
       </div>  
     </div>
   );
