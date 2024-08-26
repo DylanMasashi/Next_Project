25371c34572264bae5c7bee1394f0ede35c383c7
KianSayuri <159494737+KianSayuri@users.noreply.github.com>
2024-08-26 20:33:40 -0300

    26/08

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,31 +1,21 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Slider from "@/componentes/slider";
+import ProdutosHome from "@/componentes/produtos/produtosHome";
+import { MdLunchDining, MdLocalBar, MdDining, MdIcecream,MdFastfood } from "react-icons/md";
 
 export default function Home() {
   return (
     <div className="containerGlobal">
-      <div className={styles.containerTitulo}>
-        <h1 className={styles.txtTitulo}>Aula Next</h1>
-        <h2 >Aprendendo Next</h2>
-        <h3>Testando</h3>
-        <p>Aprendendo Next</p>
-        <div className="styles.containerImagem">
-        <Image
-          src="/avre.jpg"
-          width={100}
-          height={100}
-          alt="Imagem de uma Avre"
-          className={styles.imagemHome}
-        />
-        <Image
-          src="/avre.jpg"
-          width={500}
-          height={500}
-          alt="Imagem de uma Avre"
-          className={styles.imagemHome}
-        /> 
+      <Slider />
+        <div className={styles.tipos}>
+          <MdLunchDining className={styles.tpicon}/>
+          <MdLocalBar className={styles.tpicon}/>
+          <MdDining className={styles.tpicon}/>
+          <MdIcecream className={styles.tpicon}/>
+          <MdFastfood className={styles.tpicon}/>
         </div>
-      </div>  
+      <ProdutosHome />
     </div>
   );
 }
